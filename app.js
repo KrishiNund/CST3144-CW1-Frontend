32110244a9f949d1f21a859ededca8825f0742ad
K_Nund <ON144@live.mdx.ac.uk>
2024-10-01 13:08:47 +0400

    -Creates filter and sort area -Adds filtering and sorting logic -Makes Lesson Display Area neater

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ let app = new Vue({
         logo:"images/logo.png",
         searchData:'Hello',
         lessons:[],
+        filterOption:'',
+        sortOption:''
     },
     methods:{
         //fetch get
@@ -20,10 +22,62 @@ let app = new Vue({
                 console.log(this.lessons);
             })
             .catch(err => console.log("Error fetching lessons: ", err));
+        },
+        sort(){
+            let sortedLessons = [...this.lessons];
+            if(this.filterOption === '' || this.sortOption === '' ){
+                Swal.fire({
+                    title: 'Error',
+                    text: 'You have to choose both a filter and sort option before confirming!',
+                    icon: 'error'
+                });
+            } else if(this.filterOption === "subject"){
+
+                sortedLessons.sort((a,b) => {
+                    if (this.sortOption === "ascending"){
+                        return a.subject.localeCompare(b.subject);
+                    } else {
+                        return b.subject.localeCompare(a.subject)
+                    }
+                });
+
+            } else if(this.filterOption === "location"){
+                sortedLessons.sort((a,b) => {
+                    if (this.sortOption === "ascending"){
+                        return a.location.localeCompare(b.location);
+                    } else {
+                        return b.location.localeCompare(a.location)
+                    }
+                });
+
+            } else if(this.filterOption === "price"){
+                sortedLessons.sort((a,b) => {
+                    if(this.sortOption === "ascending"){
+                        return a.price - b.price; //ascending
+                    } else {
+                        return b.price - a.price; //descending
+                    }
+                })
+            } else if(this.filterOption === "spaces"){
+                sortedLessons.sort((a,b) => {
+                    if(this.sortOption === "ascending"){
+                        return a.spaces - b.spaces; //ascending
+                    } else {
+                        return b.spaces - a.spaces; //descending
+                    }
+                })
+            }
+
+            this.lessons = sortedLessons;
         }
+
+    },
+    computed:{
+    
     },
     mounted(){
-        this.getLessons(); //this will fetch lessons when the app loads or reloads
+        this.getLessons();
+         //this will fetch lessons when the app loads or reloads
     },
 
 })
